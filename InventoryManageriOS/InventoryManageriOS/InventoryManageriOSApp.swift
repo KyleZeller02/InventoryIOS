e84784f86cb0a92222937da021c70d4d6672dd0e
kzeller02 <kzeller02@ksu.edu>
2023-12-12 18:32:25 -0600

    Inital project created

---
+++ InventoryManageriOS/InventoryManageriOS/InventoryManageriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManageriOSApp.swift
+//  InventoryManageriOS
+//
+//  Created by Kyle Zeller on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InventoryManageriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
